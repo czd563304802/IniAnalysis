b6e40f41beed5d317fa00e43b980af59def7daae
563304802@qq.com <563304802@qq.com>
2022-06-28 09:42:49 +0800

    test_3

--- UnitTest1/unittest1.cpp
+++ UnitTest1/unittest1.cpp
@@ -2,6 +2,7 @@
 #include "CppUnitTest.h"
 #include <iostream>
 #include "E:/LIB_And_DLL/IniAnalysis/include/IniAnalysis.h"
+#define PATHNAME "E:/work/VisualStudio/.dll/IniAnalysis/test.ini"
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 using namespace std;
 namespace UnitTest1
@@ -12,7 +13,7 @@ namespace UnitTest1
 		
 		TEST_METHOD(TestMethod1)
 		{
-			FILE * fd = fopen("E:/work/VisualStudio/C++/IniAnalysis/test.ini", "r");
+			FILE * fd = fopen(PATHNAME, "r");
 			int i = 0;
 			char str[512] = {"\0"};
 			i = getLine(fd,i,str);
@@ -20,12 +21,12 @@ namespace UnitTest1
 			i = getLine(fd, i, str);
 			int n =strlen( Key(str, "Name"));
 			Assert::AreEqual(17,n);
-			Assert::AreEqual(16, MyGetInt("FF16", "Version", 123, "E:/work/VisualStudio/C++/IniAnalysis/test.ini"));
-			MyGetString("FF16","Name" ,"FF15" ,str ,512 , "E:/work/VisualStudio/C++/IniAnalysis/test.ini");
+			Assert::AreEqual(16, MyGetInt("FF16", "Version", 123, PATHNAME));
+			MyGetString("FF16", "Name", "FF15", str, 512, PATHNAME);
 			cout << str << endl;
-			Assert::AreEqual(17, MyGetString("FF16", "Name", "FF15", str, 512, "E:/work/VisualStudio/C++/IniAnalysis/test.ini"));
-			Assert::AreEqual(21, MyGetString("WOLONG", "Name", "FF15", str, 512, "E:/work/VisualStudio/C++/IniAnalysis/test.ini"));
-			Assert::AreEqual(4, MyGetString("FF16", "1", "FF15", str, 512, "E:/work/VisualStudio/C++/IniAnalysis/test.ini"));
+			Assert::AreEqual(17, MyGetString("FF16", "Name", "FF15", str, 512, PATHNAME));
+			Assert::AreEqual(21, MyGetString("WOLONG", "Name", "FF15", str, 512, PATHNAME));
+			Assert::AreEqual(4, MyGetString("FF16", "1", "FF15", str, 512, PATHNAME));
 			Assert::AreEqual(3,GetAllSection(fd, str));
 			// TODO:  在此输入测试代码
 		}
